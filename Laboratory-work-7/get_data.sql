347d841f5fc61b77bcac7f2a2fb53183f9f616b6
Mariia Sirenko <79446249+mariiasirenko2@users.noreply.github.com>
2024-11-12 22:06:42 +0200

    Update get_data.sql

--- Laboratory-work-7/get_data.sql
+++ Laboratory-work-7/get_data.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE FUNCTION get_data(attr_value TEXT)
-RETURNS TABLE (h_id INT, name TEXT, address TEXT, confidentiality_label INT) AS $$
+CREATE OR REPLACE FUNCTION get_data(attr_value VARCHAR(50))
+RETURNS TABLE (h_id INT, name VARCHAR(50), address VARCHAR(50), confidentiality_label INT) AS $$
 BEGIN
     RETURN QUERY EXECUTE 'SELECT * FROM hotel WHERE address = ''' || attr_value || '''';
 END;
